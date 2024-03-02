e3fd477ad7682c2de072f260b8fb2df53a459197
jose-valdez12 <jvaldezm1@miumg.edu.gt>
2024-03-02 12:22:44 -0600

    ejer2

--- index.php
+++ index.php
@@ -2,12 +2,19 @@
 
 $numero = 1;
 
-echo "Números impares del 1 al 50:\n";
+echo "Números impares del 0 al 50:\n";
 while ($numero <= 50) {
     if ($numero % 2 != 0) {
         echo $numero . "\n";
     }
     $numero++;
 }
-git
 
+
+$num = 0;
+
+while ($num < 30 || $num % 2 == 0) {
+    $num += 3;
+}
+
+echo "El primer número impar mayor o igual a 30 es: $num";
